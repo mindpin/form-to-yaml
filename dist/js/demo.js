4c6f9011e3867f9d53818dc16993e7016189909d
fushang318 <fushang318@gmail.com>
2015-06-05 09:59:09 +0800

    fix

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -1,6 +1,7 @@
 (function() {
   jQuery(function() {
-    return YamlToForm.load_config("../dist/yaml/config.yaml", function(ytf) {
+    // 为了兼容 gh-pages
+    return YamlToForm.load_config("/yaml-to-form/dist/yaml/config.yaml", function(ytf) {
       var instance;
       return instance = ytf.render_to(jQuery('.container'));
     });
