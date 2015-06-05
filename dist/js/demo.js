5b07a46eb5b850cc4751d5a5fcf6f792254f81bc
fushang318 <fushang318@gmail.com>
2015-06-05 09:56:57 +0800

    fix

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -1,6 +1,6 @@
 (function() {
   jQuery(function() {
-    return YamlToForm.load_config("../../dist/yaml/config.yaml", function(ytf) {
+    return YamlToForm.load_config("../dist/yaml/config.yaml", function(ytf) {
       var instance;
       return instance = ytf.render_to(jQuery('.container'));
     });
