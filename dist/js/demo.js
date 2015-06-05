5801240abd843c36ddf4186bbaeae88a1b7c53bb
fushang318 <fushang318@gmail.com>
2015-06-05 10:02:34 +0800

    fix

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -1,7 +1,7 @@
 (function() {
   jQuery(function() {
     // 为了兼容 gh-pages
-    return YamlToForm.load_config("/yaml-to-form/dist/yaml/config.yaml", function(ytf) {
+    return YamlToForm.load_config("http://www.mindpin.com/yaml-to-form/dist/yaml/config.yaml", function(ytf) {
       var instance;
       return instance = ytf.render_to(jQuery('.container'));
     });
