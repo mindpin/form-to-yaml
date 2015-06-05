94347b0af4f6b17aeabab9d38a1f130ab07c4111
fushang318 <fushang318@gmail.com>
2015-06-05 10:11:12 +0800

    fix

--- dist/js/demo.js
+++ dist/js/demo.js
@@ -1,6 +1,5 @@
 (function() {
   jQuery(function() {
-    // 为了兼容 gh-pages
     return YamlToForm.load_config("http://www.mindpin.com/yaml-to-form/dist/yaml/config1.yaml", function(ytf) {
       var instance;
       return instance = ytf.render_to(jQuery('.container'));
--- src/js/demo.coffee
+++ src/js/demo.coffee
@@ -1,3 +1,3 @@
 jQuery ->
-  YamlToForm.load_config "/dist/yaml/config.yaml", (ytf)->
+  YamlToForm.load_config "http://www.mindpin.com/yaml-to-form/dist/yaml/config1.yaml", (ytf)->
     instance = ytf.render_to jQuery('.container')
